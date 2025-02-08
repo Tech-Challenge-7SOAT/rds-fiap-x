52473a6616b3513299614edcbb91a822dcb66530
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-02-08 19:59:07 -0300

    turning rds publicly accessible

--- rds.tf
+++ rds.tf
@@ -10,7 +10,7 @@ resource "aws_db_instance" "postgres" {
   password                = var.DB_PASSWORD
   parameter_group_name    = "default.postgres16"
   skip_final_snapshot     = true
-  publicly_accessible     = false
+  publicly_accessible     = true
   vpc_security_group_ids  = [aws_security_group.fiapx_rds_sg.id]
   db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
 }
