ba1d4e08d9d0bfb932ba02e9f2e5d54de539db73
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2025-02-08 18:24:32 -0300

    fixing any configs

--- rds.tf
+++ rds.tf
@@ -1,26 +1,27 @@
-resource "aws_db_instance" "default" {
-  identifier           = "fiapx-rds"
-  allocated_storage    = 5
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "16.3"
-  instance_class       = "db.t3.micro"
-  db_name              = var.DB_NAME
-  username             = var.DB_USERNAME
-  password             = var.DB_PASSWORD
-  parameter_group_name = "default.postgres16"
-  skip_final_snapshot  = true
-  publicly_accessible  = true
-  db_subnet_group_name   = aws_db_subnet_group.fiapx_db_subnet_gp.name
-  vpc_security_group_ids = [aws_security_group.fiapx_rds_sg.id]
+resource "aws_db_instance" "postgres" {
+  identifier              = "fiapx-db"
+  allocated_storage       = 5
+  storage_type            = "gp2"
+  engine                  = "postgres"
+  engine_version          = "16.3"
+  instance_class          = "db.t3.micro"
+  db_name                 = var.DB_NAME
+  username                = var.DB_USERNAME
+  password                = var.DB_PASSWORD
+  parameter_group_name    = "default.postgres16"
+  skip_final_snapshot     = true
+  publicly_accessible     = false
+  vpc_security_group_ids  = [aws_security_group.fiapx_rds_sg.id]
+  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
+}
 
-  tags = {
-    Name = "fiapx_db_instance"
-  }
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = data.aws_subnets.default.ids
 }
 
 resource "null_resource" "db_schema" {
-  depends_on = [aws_db_instance.default]
+  depends_on = [aws_db_instance.postgres]
 
   provisioner "local-exec" {
     command = "export PGPASSWORD=${var.DB_PASSWORD}; sleep 60; psql -U ${var.DB_USERNAME} -d ${var.DB_NAME} -h ${aws_db_instance.default.address} -f ./db_schema.sql"
